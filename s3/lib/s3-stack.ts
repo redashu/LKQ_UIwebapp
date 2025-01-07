e1177b2c5ff029ed918a371f2ac816ffa332cf31
EC2 Default User <ec2-user@ip-172-31-20-248.ec2.internal>
2025-01-07 11:41:56 +0000

    updates

---
+++ s3/lib/s3-stack.ts
@@ -0,0 +1,18 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class S3Stack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // creating s3 bucket
+    const bucket = new s3.Bucket(this, 'MyFirstBucket', {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      bucketName: 'my-first-bucket-123456',
+    });
+    // create ec2 instance
+  }
+}
